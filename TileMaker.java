29a293abd9495c38d022852028a6f952c07c1bc6
ZeinBoucha <44209343+ZeinBoucha@users.noreply.github.com>
2018-12-08 23:04:24 +0000

    Add files via upload

---
+++ TileMaker.java
@@ -0,0 +1,55 @@
+public class TileMaker {
+
+    private Color PlatformColor, DisPlatformColor, HurtPlatformColor, ButtonPlatformColor, WallColor, DisWallColor;
+    private Tile Platform, DisPlatform, HurtPlatform, ButtonPlatform, Wall, DisWall;
+
+    public TileMaker(){
+        Platform = new Tile();
+        DisPlatform = new Color(, 174, 201);
+        HurtPlatform = new Color(255, 174, 201);
+        ButtonPlatform = new Color(255, 174, 201);
+        Wall = new Color(255, 174, 201);
+        DisWall = new Color(255, 174, 201);
+        (double x, double y, double height, double width, boolean hurt, boolean solid)
+    }
+
+    public Tile getPlatfrom()
+    {
+        Tile platform = new Tile()
+    }
+
+    public Tile getDisPlatform()
+    {
+
+    }
+
+    public Tile getHurtPlatfrom()
+    {
+
+    }
+
+    public Tile getButtonPlatfrom()
+    {
+
+    }
+
+    public Tile getWall()
+    {
+
+    }
+
+    public Tile getDisWall()
+    {
+
+    }
+
+    public Tile getPortal()
+    {
+
+    }
+
+
+
+}
+
+
