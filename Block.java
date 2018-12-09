002d2ef9bf6ec155f24e379e729c2347179ec535
ZeinBoucha <44209343+ZeinBoucha@users.noreply.github.com>
2018-12-09 07:29:25 +0000

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,44 @@
+import processing.core.PApplet;
+import processing.core.PShape;
+
+import java.awt.*;
+
+//import Java.core.library.*;
+
+public class Block extends PApplet {
+
+    PShape tile;
+    double x, y, h, w;
+    Color colour;
+    public Block(double x, double y, double h, double w, Color colour ){
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+        this.colour = colour;
+        setup();
+    }
+
+    public Color getColour(){ return colour;}
+
+    public void setup() {
+        rectMode(CORNERS);
+        tile = createShape(RECT, Math.round(x), Math.round(y), Math.round(h), Math.round(w));
+        tile.setStroke(color(colour.getRed(), colour.getGreen(), colour.getBlue()));
+        tile.setFill(color(colour.getRed(), colour.getGreen(), colour.getBlue()));
+
+    }
+
+    public PShape getTile(){
+
+            return tile;}
+
+    public double getXCoord(){ return x;}
+
+    public double getYCoord(){ return y;}
+
+    public double getWid(){ return w;}
+
+    public double getHeight(){ return h;}
+
+}
--- Tile.java
+++ Tile.java
@@ -1,18 +1,18 @@
+import java.awt.*;
+
 public class Tile{
 
     private double x, y, width,height ;
     private boolean hurt, faze;
-    private BufferedImage image;
-    private Rectangle2D.Double platformBlock;
+    private Color color;
 
-    public Tile(double x, double y, double height, double width, boolean hurt, boolean solid, Color color){
+    public Tile(double x, double y, double height, double width, Color color){
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
-        this.hurt = hurt;
-        this.solid = solid;
-        platformBlock = new Rectangle2D.Double(x, y, width, height);
+        //this.hurt = hurt;
+        this.color = color;
 
     }
 
@@ -36,31 +36,18 @@ public class Tile{
         return width;
     }
 
-    public boolean getHurt()
-    {
-        return hurt;
+    public Color getColor() { return color; }
+
+  //  public boolean getHurt()
+   // {
+    //    return hurt;
+  //  }
+
+
+    public double[][] getCP(){
+        double[][] arr = {{x, y},  {x+ width, y},{x, y + height},{x + width, y + height}};;
+        return arr;
     }
-
-    public boolean getIsSolid()
-    {
-        return faze;
-    }
-
-    public void toggleSolid()
-    {
-        Solid = !Solid;
-    }
-
-    public double[4][2] getCP()
-    {
-        double arr[4][2] = new double[4][2];
-        arr[0] = new double[2]{x, y};
-        arr[1] = new double[2]{x+ width, y};
-        arr[2] = new double[2]{x, y + height};
-        arr[3] = new double[2]{x + width, y + height};
-
-    }
-
 
 
 
