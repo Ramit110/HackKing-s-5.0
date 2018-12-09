e5fec83d1e88ddc53744775fb18ab2a84f7f5350
Ravi Ghaghada <ravighaghada28@gmail.com>
2018-12-09 02:13:14 +0000

    Modified Tiles, Created TileMaker

--- src/Imageextractor.java
+++ src/Imageextractor.java
@@ -6,15 +6,6 @@ public class Imageextractor {
     ArrayList <Tile> tiles = new ArrayList<>();
 
     public static void createMap(String filename){
-        BufferedImage img = ImageIO.read(new File(filename));
 
-        for (int x=0; x<img.getWidth(); x++)
-            for (int y=0; y<img.getHeight(); y++){
-                Color color = new Color (img.getRGB(x, y));
-                int r1 = color.getRed();
-
-                new Tile ()
-
-            }
     }
 }
---
+++ src/Tile.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+
+public class Tile{
+
+    private double x, y, width,height ;
+    private boolean hurt, faze;
+    private Color color;
+
+    public Tile(double x, double y, double height, double width, Color color){
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.hurt = hurt;
+        this.color = color;
+
+    }
+
+    public double getXCoordinate()
+    {
+        return x;
+    }
+
+    public double getYCoordinate()
+    {
+        return y;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public double getWidth()
+    {
+        return width;
+    }
+
+    public boolean getHurt()
+    {
+        return hurt;
+    }
+
+
+    public double[][] getCP(){
+        double[][] arr = {{x, y},  {x+ width, y},{x, y + height},{x + width, y + height}};;
+        return arr;
+    }
+
+
+
+}
